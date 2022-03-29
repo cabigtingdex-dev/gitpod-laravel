c996f39acd4eefdf55a3420b6c0af6c2edf98cdb
cabigtingdex-dev <carts.mailer@gmail.com>
2022-03-29 12:00:49 +0000

    Created Book factory

---
+++ laravue/database/factories/BookFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->word(),
+            'author' => $this->faker->name(),
+            'category' => $this->faker->word(),
+            'description' => $this->faker->text(50),
+            'publishing_house' => $this->faker->company(),
+            'publishing_date' => now(),
+        ];
+    }
+}
