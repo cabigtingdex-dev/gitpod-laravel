c5869f310e0e21bd4f936cac304663383830e82b
cabigtingdex-dev <carts.mailer@gmail.com>
2022-03-29 12:39:35 +0000

    Created request for updating book

---
+++ laravue/app/Http/Requests/UpdateBookRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['string', 'min:5', 'max:100'],
+            'author' => ['string', 'min:5', 'max:100'],
+            'category' => ['string', 'min:5', 'max:100'],
+            'publishing_house' => ['string', 'min:5', 'max:100'],
+            'publishing_date' => ['date']
+        ];
+    }
+}
