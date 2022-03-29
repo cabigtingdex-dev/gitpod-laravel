4c289eef1e00f7e25c38ba73762bf5854150b391
cabigtingdex-dev <carts.mailer@gmail.com>
2022-03-29 11:57:06 +0000

    Created books table migration

--- laravue/database/migrations/2022_03_29_114857_create_books_table.php
+++ laravue/database/migrations/2022_03_29_114857_create_books_table.php
@@ -15,6 +15,11 @@ class CreateBooksTable extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->string('author', 100);
+            $table->string('category', 100);
+            $table->string('publishing_house', 100);
+            $table->date('publishing_date');
             $table->timestamps();
         });
     }
