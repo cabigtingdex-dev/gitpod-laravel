5cb77825439599f27e6588a312a110ab4b805513
cabigtingdex-dev <carts.mailer@gmail.com>
2022-04-01 18:32:33 +0000

    Added logic for batchStore method

--- laravue/app/Http/Controllers/API/BookBatchController.php
+++ laravue/app/Http/Controllers/API/BookBatchController.php
@@ -4,13 +4,35 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Book;
+use App\Http\Resources\BookResource;
+use Illuminate\Support\Arr;
 
 class BookBatchController extends Controller
 {
    
     public function batchStore(Request $request)
     {
-        //
+        $newBooks = $request->newBooks;
+
+        $bookCount = count($newBooks);
+
+        $bookIds = [];
+
+        for($i = 0; $i < $bookCount; $i++)
+        {
+            $book = Book::create([
+                'title' => $newBooks[$i]['title'],
+                'author' => $newBooks[$i]['author'],
+                'category' => $newBooks[$i]['category'],
+                'publishing_house' => $newBooks[$i]['publishing_house'],
+                'publishing_date' => $newBooks[$i]['publishing_date']
+            ]);
+
+            $bookIds = Arr::prepend($bookIds, $book->id);
+        }
+
+        return BookResource::collection(Book::find($bookIds));
     }
 
     
