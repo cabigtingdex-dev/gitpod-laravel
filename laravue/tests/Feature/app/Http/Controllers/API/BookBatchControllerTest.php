673e4c0b628d54cadd31c3e60f6802e0bdede3cb
cabigtingdex-dev <carts.mailer@gmail.com>
2022-04-03 09:26:35 +0000

    Added test for batchDestroy method

--- laravue/tests/Feature/app/Http/Controllers/API/BookBatchControllerTest.php
+++ laravue/tests/Feature/app/Http/Controllers/API/BookBatchControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\app\Http\Controllers\API;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Book;
 
 class BookBatchControllerTest extends TestCase
 {
@@ -13,10 +14,23 @@ class BookBatchControllerTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    // public function test_example()
+    // {
+    //     $response = $this->get('/');
 
-        $response->assertStatus(200);
+    //     $response->assertStatus(200);
+    // }
+
+    public function test_if_batch_destroy_endpoint_responds_204()
+    {
+        $books = Book::factory()->count(3)->create();
+
+        $response = $this->delete('/api/books/batch', [
+            'bookIds' => [1, 2, 3]
+        ]);
+
+        $this->assertDatabaseCount('books', 0);
+
+        $response->assertStatus(204);        
     }
 }
