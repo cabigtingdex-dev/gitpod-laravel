985091033edde37ec8b941bca8a79ff026c2dea4
cabigtingdex-dev <carts.mailer@gmail.com>
2022-04-07 14:05:05 +0000

    Defined belongsToMany relationship with books table

--- laravue/app/Models/Category.php
+++ laravue/app/Models/Category.php
@@ -12,4 +12,9 @@ class Category extends Model
     protected $fillable = [
         'category_name'
     ];
+
+    public function books()
+    {
+        return $this->belongsToMany(Book::class);
+    }
 }
