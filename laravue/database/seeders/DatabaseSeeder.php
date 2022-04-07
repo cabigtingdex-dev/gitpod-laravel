2e0b4f56df40c4a6e9d91c0d2ae66d07c4af2508
cabigtingdex-dev <carts.mailer@gmail.com>
2022-04-07 14:56:32 +0000

    Called CategorySeeder class

--- laravue/database/seeders/DatabaseSeeder.php
+++ laravue/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call([BookSeeder::class]);
+        $this->call([
+            BookSeeder::class,
+            CategorySeeder::class            
+        ]);
     }
 }
